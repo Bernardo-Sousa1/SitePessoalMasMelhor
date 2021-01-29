10dd5d658e3df8fddf5f879324b526a62012b93b
Bernardo-Sousa1 <bernas_17_cs@hotmail.com>
2021-01-29 12:19:01 +0000

    Ligação das views

---
+++ SitePessoalMasMelhor/Migrations/20210129112529_Contactos.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SitePessoalMasMelhor.Migrations
+{
+    public partial class Contactos : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Contacto",
+                columns: table => new
+                {
+                    ContactoId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    Mensagem = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contacto", x => x.ContactoId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Contacto");
+        }
+    }
+}
